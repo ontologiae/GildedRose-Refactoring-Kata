23c06eafbdd87d571c340baab43de05001599ebb
Pierre-Alex <ontologiae@gmail.com>
2020-09-24 18:23:31 +0200

    Refactor OK

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import java.util.Arrays; 
+
 class GildedRose {
     Item[] items;
 
@@ -7,8 +9,75 @@ class GildedRose {
         this.items = items;
     }
 
+
+		public Item[] getItems() {
+			return items;
+		}
+
     public void updateQuality() {
-			// update items i set quality = quality - 1 where i.name not in ('Aged...','Backstage','Sulfuras') and quality > 0
+			// En terme de perf, il vaudrai mieux discriminer en fonction de la quality et sellIn mais en terme de lisibilité avec name
+
+			for (int j = 0; j < items.length; j++) {
+				Item i = items[j];
+				switch (i.name) {
+					case "Aged Brie" : 
+						// B update items i set quality = quality +1 where i.name in ('Aged...','Backstage') and (i.quality < 50 )
+						// E update items i set i.sellIn = i.sellIn - 1 where i.name <> 'Sulfuras...'
+						// H update items i set i.quality = quality + 1 where i.name in ('Aged...') and i.sellIn < 0 and i.quality < 50
+						items[j].sellIn = i.sellIn - 1;
+						if (i.quality < 50) {
+							items[j].quality = i.quality+1;
+							if (i.sellIn < 6)
+								items[j].quality = i.quality+1;
+						}
+						break;
+					case "Backstage passes to a TAFKAL80ETC concert" : 
+						// B update items i set quality = quality +1 where i.name in ('Aged...','Backstage') and (i.quality < 50 )
+						//C update items i set quality = quality +1 where i.name in ('Backstage') and i.quality < 50 and i.sellIn < 11
+						//D update items i set quality = quality +1 where i.name in ('Backstage') and i.quality < 50 and i.sellIn < 6
+					  // E update items i set i.sellIn = i.sellIn - 1 where i.name <> 'Sulfuras...'
+						// //  G update items i set i.quality = 0 where i.name  in ('Backstage...')  and i.sellIn < 0
+						// E
+						items[j].sellIn = i.sellIn - 1;
+						// 
+						if (i.quality < 50) {
+							items[j].quality = i.quality+1;
+							if (i.sellIn < 11)
+								items[j].quality = i.quality+1;
+							if (i.sellIn < 6)
+								items[j].quality = i.quality+1;
+						}
+						//G
+						if (i.sellIn < 0)
+							items[j].quality = 0;
+						break;
+					case "Sulfuras, Hand of Ragnaros" : 
+						//if (i.quality < 50)
+						//	items[j].quality = i.quality+1;
+						break;
+					default:
+						//A update items i set quality = quality - 1 where i.name not in ('Aged...','Backstage') and (quality > 0 and i.name <> 'Sulfuras..')
+						//// E update items i set i.sellIn = i.sellIn - 1 where i.name <> 'Sulfuras...'
+						//// F update items i set quality = quality - 1 where i.name not in ('Aged...','Backstage','Sulfuras') and quality > 0 and i.sellIn < 0
+						//E
+						items[j].sellIn = i.sellIn - 1;
+						//A
+						if (i.quality > 0) {
+							items[j].quality = i.quality-1;
+							//F
+							if(i.sellIn < 0)
+								items[j].quality = i.quality-1;
+						}							
+						break;
+
+				}
+			}
+		}
+
+
+			
+	     public void updateQualityRef() {
+	 		// A update items i set quality = quality - 1 where i.name not in ('Aged...','Backstage') and (quality > 0 and i.name <> 'Sulfuras..')
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -18,12 +87,13 @@ class GildedRose {
                     }
                 }
             } else {
-							// update items i set quality = quality +1 where i.name in ('Aged...','Backstage') and i.quality < 50
-							// update items i set quality = quality +1 where i.name in ('Aged...','Backstage') and i.quality < 50 and i.sellIn < 6
+							// B update items i set quality = quality +1 where i.name in ('Aged...','Backstage') and (i.quality < 50 )
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) { // ce cas ne peut survenir
+										//C update items i set quality = quality +1 where i.name in ('Backstage') and i.quality < 50 and i.sellIn < 11
+										//D update items i set quality = quality +1 where i.name in ('Backstage') and i.quality < 50 and i.sellIn < 6
+                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) { 
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -36,15 +106,17 @@ class GildedRose {
                             }
                         }
                     }
+
+
                 }
             }
 
-						// update items i set i.sellIn = i.sellIn - 1 where i.name = 'Sulfuras...'
+						// E update items i set i.sellIn = i.sellIn - 1 where i.name <> 'Sulfuras...'
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
-						// update items i set quality = quality - 1 where i.name not in ('Aged...','Backstage','Sulfuras') and quality > 0 and i.sellIn < 0
+						// F update items i set quality = quality - 1 where i.name not in ('Aged...','Backstage','Sulfuras') and quality > 0 and i.sellIn < 0
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -53,10 +125,10 @@ class GildedRose {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
-                    } else { //  update items i set i.quality = 0 where i.name not in ('Aged...')  and i.sellIn < 0
+                    } else { //  G update items i set i.quality = 0 where i.name  in ('Backstage...')  and i.sellIn < 0
                         items[i].quality = items[i].quality - items[i].quality;
                     }
-                } else { // update items i set i.quality = quality + 1 where i.sellIn < 0 and i.quality < 50
+                } else { // H update items i set i.quality = quality + 1 where i.name in ('Aged...') and i.sellIn < 0 and i.quality < 50
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
                     }
@@ -64,4 +136,14 @@ class GildedRose {
             }
         }
     }
+
+
+  public GildedRose[] onAideEvoSuite(GildedRose g1)  {
+		GildedRose tmp = new GildedRose(Arrays.copyOf(g1.items,g1.items.length));
+
+		g1.updateQuality();
+		return new GildedRose[] {g1,tmp};
+		//return res;
+	}
+
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,160 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+//import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
+import com.gildedrose.Item;
+import com.gildedrose.GildedRose;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class GildedRoseTest {
+public class GildedRoseTest {
+
+
+	public GildedRoseTest() {
+	}
+
+
+	  @Test
+    public void decrease_quality_and_sellin_of_default_items() {
+        Item[] givenDefaultItems = new Item[]{new Item("default", 30, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("default", 29, 19)};
+        GildedRose gildedRose = new GildedRose(givenDefaultItems);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
 
     @Test
-    void foo() {
+    public void decrease_quality_twice_as_fast_of_default_items_when_sellin_is_negative() {
+        Item[] givenDefaultItems = new Item[]{new Item("default", -1, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("default", -2, 18)};
+        GildedRose gildedRose = new GildedRose(givenDefaultItems);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+
+    /*@Test
+    public void decrease_quality_twice_as_fast_for_conjured() {
+        Item[] givenDefaultItems = new Item[]{new Item("Conjured Mana Cake", 30, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Conjured Mana Cake", 29, 18)};
+        GildedRose gildedRose = new GildedRose(givenDefaultItems);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+
+    @Test
+    public void decrease_quality_twice_as_fast_of_conjured_when_sellin_is_negative() {
+        Item[] givenDefaultItems = new Item[]{new Item("Conjured Mana Cake", -1, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Conjured Mana Cake", -2, 16)};
+        GildedRose gildedRose = new GildedRose(givenDefaultItems);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }*/
+   
+
+		@Test
+    public void not_modify_sulfuras() {
+        Item[] givenDefaultItems = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 30, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 30, 20)};
+        GildedRose gildedRose = new GildedRose(givenDefaultItems);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+
+    @Test
+    public void not_modify_sulfuras_even_with_negative_sellin() {
+        Item[] givenDefaultItems = new Item[]{new Item("Sulfuras, Hand of Ragnaros", -1, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Sulfuras, Hand of Ragnaros", -1, 20)};
+        GildedRose gildedRose = new GildedRose(givenDefaultItems);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+
+    @Test
+    public void increase_quality_of_aged_brie() {
+        Item[] givenAnAgedBrie = new Item[]{new Item("Aged Brie", 30, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Aged Brie", 29, 21)};
+        GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+
+    @Test
+    public void increase_quality_twice_as_fast_of_aged_brie_when_sellin_is_low() {
+        Item[] givenAnAgedBrie = new Item[]{new Item("Aged Brie", -1, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Aged Brie", -2, 22)};
+        GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+
+    @Test
+    public void increase_quality_of_backstage() {
+        Item[] givenAnAgedBrie = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 30, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 29, 21)};
+        GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+
+    @Test
+    public void increase_quality_twice_as_fast_of_backstage_when_sellin_is_low() {
+        Item[] givenAnAgedBrie = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 9, 22)};
+        GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+    @Test
+    public void increase_quality_three_times_as_fast_of_backstage_when_sellin_is_very_low() {
+        Item[] givenAnAgedBrie = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 4, 23)};
+        GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+    @Test
+    public void delete_quality_of_backstage_when_sellin_is_negative() {
+        Item[] givenAnAgedBrie = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20)};
+        Item[] expectedDefaultItems = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", -2, 0)};
+        GildedRose gildedRose = new GildedRose(givenAnAgedBrie);
+        gildedRose.updateQuality();
+        assertThat(gildedRose.getItems())
+                .usingFieldByFieldElementComparator()
+                .containsExactly(expectedDefaultItems);
+    }
+
+    @Test
+    public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
+
+
 
 }
